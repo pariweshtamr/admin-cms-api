ca41c651a5eb7c8a9b9fbe31e6055761a0fc80dd
Pariwesh Tamrakar <tamrpariwesh@gmail.com>
2021-09-11 13:49:20 +1000

    bcrypt hashing password

---
+++ helpers/bcrypt.helper.js
@@ -0,0 +1,12 @@
+import bcrypt from 'bcrypt'
+
+const saltRounds = 10
+
+export const hashPassword = plainPass => {
+    return bcrypt.hashSync(plainPass, saltRounds)
+}
+
+//COMPARE PASSWORD
+export const comparePassword = (plainPass, hashPassFromDB) => {
+    return bcrypt.compareSync(plainPass, hashPassFromDB)
+}
--- routers/userRouter.js
+++ routers/userRouter.js
@@ -2,6 +2,7 @@ import express from 'express'
 const Router = express.Router()
 import {createUser} from '../models/user-model/User.model.js'
 import { createAdminUserValidation } from '../middlewares/formValidation.middleware.js'
+import {hashPassword} from '../helpers/bcrypt.helper.js'
 
 Router.all("/", (req, res, next) => {
     console.log('from user router')
@@ -14,6 +15,11 @@ Router.post ("/", createAdminUserValidation, async (req, res) => {
         // TODO
         
         // encrypt password
+        const hashPass = hashPassword(req.body.password)
+        if(hashPass){
+        
+        req.body.password=hashPass
+        console.log(hashPass)
 
         const result = await createUser(req.body)
 
@@ -24,6 +30,7 @@ Router.post ("/", createAdminUserValidation, async (req, res) => {
                 state: 'success',
                 message: 'New user has been created successfully! We hav sent an email confirmation to your email, please check and follow the instructions to verify and activate your account'
             })
+        }
         }
 
         res.json({
